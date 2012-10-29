cff42a1683857733421a37732fb075c99a7019e0
lukehoggett <luke.hoggett@gmail,com>
2012-10-29 17:18:33 +1100

    Refined fading algorithm

--- Single_LED_color_space/Single_LED_color_space.ino
+++ Single_LED_color_space/Single_LED_color_space.ino
@@ -8,105 +8,97 @@ int SDI = 3;
 #define STRIP_LENGTH 1 // Number of RGBLED modules connected
 long strip_colours[STRIP_LENGTH];
 
-int previousHue= 0;
-int stepSize = 5;
+int currentHue;
+int stepSize = 1;
 
 
 void setup()
 {
-  //Serial.begin(9600);
-  randomSeed(analogRead(0));
+  Serial.begin(9600);
+  randomSeed(analogRead(0) + analogRead(2));
+  currentHue = random(0, 255);
   pinMode(SDI, OUTPUT);
   pinMode(CKI, OUTPUT);
 
 }
 
-float dist(float a, float b, float c, float d)
-{
-  return sqrt((c-a)*(c-a)+(d-b)*(d-b));
-}
-
 void loop()
 {
-  // get a random hue
-  int hue = random(0, 255);
-  //int hue = 20;
-  /*Serial.print("Previous: ");
-  Serial.println(previousHue);
-  Serial.print("Hue: ");
-  Serial.println(hue);*/
-  
-  
-    
-  // establish the number of steps between the current and previous hue
-  int numSteps;
-  if (previousHue > hue)
+  // get a random nextHue
+  int nextHue = random(0, 255);
+  int distance;
+  boolean up;
+  if (currentHue > nextHue)
   {
-    numSteps = (hue - previousHue + 360) / stepSize;  
+    up = false;
+    distance = nextHue - currentHue;
+  }
+  else if (nextHue >= currentHue) 
+  {
+    up = true;
+    distance = currentHue - nextHue;    
+  }
+  
+  
+  // establish the number of steps between the current and previous nextHue
+  int numSteps = distance / stepSize;
+  if (currentHue > nextHue)
+  {
+    numSteps = (nextHue - currentHue + 256) / stepSize;  
   }
   else
   {
-    numSteps = (hue - previousHue) / stepSize;
+    numSteps = (nextHue - currentHue) / stepSize;
   }
   
-  /*Serial.print("NumSteps: ");
-  Serial.println(numSteps);*/
-  // set up array to contain the hue steps
-  unsigned long steps[numSteps];
-  
-  int hueStep;
-
-  for (int i = 0; i < numSteps; i++)
+  nextHue = numSteps * stepSize;
+  int hue;
+  if (up)
   {
-    hueStep = (i * stepSize) + previousHue; 
-    if (hueStep > 360)
+    for (int i = 0; i < numSteps; i = i + stepSize)
     {
-      hueStep = hueStep - 360;
-
+      hue = currentHue + i;
+      ColorRGB colorRGB;
+      ColorHSV colorHSV = {(unsigned char)(hue), 255, 255};
+      color.HSVtoRGB(&colorRGB, &colorHSV);
+    
+      unsigned long rgbVal = ((unsigned long)colorRGB.r << 16 | colorRGB.g << 8 | colorRGB.b) & 0xFFFFFF;
+    
+      Serial.println(rgbVal, HEX);
+      write(rgbVal);
+    
+      delay(100);
     }
+  }
+  else
+  {
+    for (int i = 0; i < numSteps; i = i + stepSize)
+    {
+      hue = currentHue - i;
+      ColorRGB colorRGB;
+      ColorHSV colorHSV = {(unsigned char)(hue), 255, 255};
+      color.HSVtoRGB(&colorRGB, &colorHSV);
     
-    /*Serial.print("Hue Step: ");
-    Serial.println(hueStep);*/
+      unsigned long rgbVal = ((unsigned long)colorRGB.r << 16 | colorRGB.g << 8 | colorRGB.b) & 0xFFFFFF;
     
-    ColorRGB colorRGB;
-    ColorHSV colorHSV = {(unsigned char)(map(hueStep, 1, 360, 0, 255)), 255, 255};
-    color.HSVtoRGB(&colorRGB, &colorHSV);
-    /*Serial.print("RGB: ");
-    Serial.print(colorRGB.r);
-    Serial.print(", ");
-    Serial.print(colorRGB.g);
-    Serial.print(", ");
-    Serial.println(colorRGB.b);*/
-//    unsigned long rgbVal = (map(colorRGB.r, 0, 127, 0, 255) << 16 | map(colorRGB.g, 0, 127, 0, 255) << 8 | map(colorRGB.b, 0, 127, 0, 255)) & 0xFFFFFF;
-    unsigned long rgbVal = (colorRGB.r << 16 | colorRGB.g << 8 | colorRGB.b) & 0xFFFFFF;
-    /*Serial.print("RGB: ");
-    Serial.println(rgbVal, HEX);*/
-    /*Serial.print("Value: ");
-    Serial.println((unsigned char)((value) * 128)&0xff);*/
-    steps[i] = rgbVal;
-
+      Serial.println(rgbVal, HEX);
+      write(rgbVal);
+    
+      delay(100);
+    }
   }
   
-  //post_frame(0);
   
-  for (int i = 0; i < numSteps; i++)
-  {
-    doFade(i, steps, numSteps);
-  }//
-  previousHue = hue;
-  //Serial.println("=====");
-  delay(500);
+  currentHue = nextHue;
+
+  delay(1500);
 }
 
 
-void doFade (int colour_id, unsigned long *steps, int numSteps) {
-  
-  
-  for(int LED_number = 0; LED_number < STRIP_LENGTH; LED_number++)
-  {
-    unsigned long this_led_colour = steps[colour_id]; //24 bits of color data
-    //Serial.print("this LED colour: ");
-    //Serial.println(this_led_colour, HEX);
+void write(unsigned long colour)
+{
+  unsigned long this_led_colour = colour; //24 bits of color data
+    
     for(byte colour_bit = 23 ; colour_bit != 255 ; colour_bit--) {
       //Feed color bit 23 first (red data MSB)
 
@@ -127,12 +119,7 @@ void doFade (int colour_id, unsigned long *steps, int numSteps) {
       
     }
     
-    delay(500);
-    
-  }
-  
-
-  //Pull clock low to put strip into reset/post mode
+    //Pull clock low to put strip into reset/post mode
   digitalWrite(CKI, LOW);
   delayMicroseconds(500); //Wait for 500us to go into reset
-}
+} 
